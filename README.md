20a7e9d3170b6cccd6b966fd4680ae3d88922557
Nagesh Pai <nagesh.pai@gmail.com>
2021-08-22 22:18:47 +0530

    Added Formulas and basic score display

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # framingham-calculator
 
-Simple 10 year Coronary Disease risk calculator based on Framingham Study 
+Simple 10 year Coronary Disease risk calculator based on [Framingham Study](https://www.ahajournals.org/doi/full/10.1161/01.cir.97.18.1837) .  
+
--- src/js/framingham-calculator.js
+++ src/js/framingham-calculator.js
@@ -16,6 +16,9 @@ function handleSubmit(event) {
     console.log(`age is ${age}`);
 
     const crs = calculate(form);
+
+    const result = document.querySelector("#result");
+    result.innerHTML = `<h2> Your Framingham Cardiac Risk Score is ${crs}</h2>`;
 }
 
 function calculate(frm) {
@@ -28,23 +31,146 @@ function calculate(frm) {
     const bp = frm['bp'].value;
     const diabetic = frm['diabetic'].value;
     const smoke = frm['smoke'].value;
-    let crs = 0;
+    let crs = 0; // Coronary Heard disease risk score
 
-   switch(gender) {
+    // check if age is greater than 30 and less than 75
+
+   switch( gender ){
 
         case 'male':
-            //calculation for male respondent
-            if ( age < 34 ) {
+            // Calculation for male respondent
+            if( age < 34 ){ // Add score for age
+                    crs += -1;
+            } else if( age <= 39 ){
+                crs += 0;
+            } else if( age <= 44 ){
+                crs += 1;
+            } else if( age <= 49 ){
+                crs += 2;
+            } else if( age <= 54 ){
+                crs += 3;
+            } else if( age <= 59 ){
+                crs += 4;
+            } else if( age <= 64 ){
+                crs += 5;
+            } else if( age <= 69 ){
+                crs += 6;
+            } else if( age <= 74 ){
+                crs += 7;
+            };
 
-                else if (age < )
-            }
+            if( tc < 160 ){ // Add total cholestrol score
+                crs += -3;
+            } else if( tc <= 199 ){
+                crs += 0;
+            } else if( tc <= 239 ){
+                crs += 1;
+            } else if ( tc <= 279 ){
+                crs += 2;
+            } else if( tc > 280 ){
+                crs += 3;
+            };
 
+            if( hdl < 35 ){ // Add score for HDL
+                crs += 2;
+            } else if( hdl <= 44 ){
+                crs += 1;
+            } else if( hdl <= 49 ){
+                crs += 0;
+            } else if( hdl <= 59 ){
+                crs += 0;
+            } else if( hdl >= 60 ){
+                crs += -2; 
+            };
 
+            if( bp < 120 ){ // Add score for systolic blood pressure
+                crs += 0;
+            } else if( bp <= 129 ){
+                crs += 0;
+            } else if( bp <= 139 ){
+                crs += 1;
+            } else if( bp <= 159 ){
+                crs += 2;
+            } else if( bp >= 160 ){
+                crs += 3;
+            };
+
+            if( diabetic === "yes" ){ // Add diabetes score
+                crs += 2;
+            };
+
+            if( smoke === "yes" ){ // Add smoking score
+                crs += 2;
+            };
             break;
 
         case 'female':
             //calculation for female respondent
+            if( age < 34 ){ // Add score for age
+                crs += -9;
+            } else if( age <= 39 ){
+                crs += -4;
+            } else if( age <= 44 ){
+                crs += 0;
+            } else if( age <= 49 ){
+                crs += 3;
+            } else if( age <= 54 ){
+                crs += 6;
+            } else if( age <= 59 ){
+                crs += 7;
+            } else if( age <= 64 ){
+                crs += 8;
+            } else if( age <= 69 ){
+                crs += 8;
+            } else if( age <= 74 ){
+                crs += 8;
+            };
+
+        if( tc < 160 ){ // Add total cholestrol score
+                crs += -2;
+            } else if( tc <= 199 ){
+                crs += 0;
+            } else if( tc <= 239 ){
+                crs += 1;
+            } else if ( tc <= 279 ){
+                crs += 1;
+            } else if( tc > 280 ){
+                crs += 3;
+            };
+
+            if( hdl < 35 ){ // Add score for HDL
+                crs += 5;
+            } else if( hdl <= 44 ){
+                crs += 2;
+            } else if( hdl <= 49 ){
+                crs += 1;
+            } else if( hdl <= 59 ){
+                crs += 0;
+            } else if( hdl >= 60 ){
+                crs += -3; 
+            };
+
+        if( bp < 120 ){ // Add score for systolic blood pressure
+                crs += -3;
+            } else if( bp <= 129 ){
+                crs += 0;
+            } else if( bp <= 139 ){
+                crs += 0;
+            } else if( bp <= 159 ){
+                crs += 2;
+            } else if( bp >= 160 ){
+                crs += 3;
+            };
+
+        if( diabetic === "yes" ){ // Add diabetes score
+                crs += 4;
+            };
+
+        if( smoke === "yes" ){ // Add smoking score
+                crs += 2;
+            };
             break;
    } 
+   return crs;
 
 }
